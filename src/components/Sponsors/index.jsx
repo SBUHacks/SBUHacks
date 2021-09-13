76187209e119b89eb9306f688209b9e8b955bcf2
Jenny Bao <jennybao1004@gmail.com>
2021-09-12 22:28:59 -0400

    Add Lazy Load & Fix Sticker Mule Icon

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -5,7 +5,7 @@ import SchifferIcon from '../../assets/sponsors/Schiffer_Icon.svg';
 import echoIcon from '../../assets/sponsors/echoAR_Icon.png';
 import AVILogo from '../../assets/sponsors/AVI_Icon.png';
 import AlumniLogo from '../../assets/sponsors/Alumni_Icon.png';
-import StickerLogo from '../../assets/sponsors/StickerMule_Icon.png';
+import StickerLogo from '../../assets/sponsors/StickerMule_Icon.svg';
 import GoogleLogo from '../../assets/sponsors/GoogleCloud_Icon.png';
 
 // The Sponsors page
@@ -23,7 +23,7 @@ function Sponsors() {
           <img src={SchifferIcon} className="partner-logo-stacked" alt="schiffer-logo" />
           <img src={AVILogo} className="partner-logo-stacked" alt="applied-visons-logo" />
           <a className="partner-logo-stacked" href="https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral" target="_blank" rel="noreferrer">
-            <img src={StickerLogo} id="sticker-mule-logo" alt="sticker-mule-logo" />
+            <img src={StickerLogo} alt="sticker-mule-logo" />
           </a>
           <img src={AlumniLogo} className="partner-logo" alt="sbu-alumni-logo" />
           <img src={echoIcon} className="partner-logo" alt="echoAR-logo" />
