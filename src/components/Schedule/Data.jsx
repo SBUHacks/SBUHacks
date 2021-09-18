41c4955a2fb50053d2b85d7c82528346b23f2c91
Jenny Bao <jennybao1004@gmail.com>
2021-09-18 18:51:49 -0400

    Update schedule

--- src/components/Schedule/Data.jsx
+++ src/components/Schedule/Data.jsx
@@ -11,31 +11,33 @@ export const FIRST_EVENTS = [
     endTime: '2021-09-24T18:30:00.000-04:00',
   },
   {
-    name: '06:00PM - START HACKING!!!',
-    endTime: '2021-09-24T19:00:00.000-04:00',
+    name: '07:00PM - START HACKING!!!',
+    endTime: '2021-09-24T19:01:00.000-04:00',
   },
   {
-    name: '07:00PM - Softheon Event',
+    name: '07:00PM - How to Shine During Recruitment with Softheon',
     endTime: '2021-09-24T20:00:00.000-04:00',
   },
-  /*
   {
-    name: '08:00PM - ',
+    name: '08:00PM - !Light with MLH',
     endTime: '2021-09-24T21:00:00.000-04:00',
   },
+  /*
   {
     name: '09:00PM - ',
     endTime: '2021-09-24T22:00:00.000-04:00',
   },
-  {
-    name: '10:00PM - ',
-    endTime: '2021-09-24T23:00:00.000-04:00',
-  },
   */
   {
-    name: '11:00PM - Among Us',
+    name: '10:00PM - Among Us',
+    endTime: '2021-09-24T23:00:00.000-04:00',
+  },
+  /*
+  {
+    name: '11:00PM - ',
     endTime: '2021-09-25T00:00:00.000-04:00',
   },
+  */
 ];
 
 export const SECOND_EVENTS = [
@@ -64,11 +66,11 @@ export const SECOND_EVENTS = [
   },
   */
   {
-    name: '08:00AM - Mental Health Event',
+    name: '08:00AM - Chess Tournament',
     endTime: '2021-09-25T09:00:00.000-04:00',
   },
   {
-    name: '09:00AM - Chess Tournament',
+    name: '09:00AM - AutoCAD Workshop: Introduction to 3D Design',
     endTime: '2021-09-25T10:00:00.000-04:00',
   },
   /*
@@ -76,10 +78,12 @@ export const SECOND_EVENTS = [
     name: '10:00AM - ',
     endTime: '2021-09-25T11:00:00.000-04:00',
   },
+  */
   {
-    name: '11:00AM - ',
+    name: '11:00AM - Intro to Capture the Flag with the Cyber Security club',
     endTime: '2021-09-25T12:00:00.000-04:00',
   },
+  /*
   {
     name: '12:00PM - ',
     endTime: '2021-09-25T13:00:00.000-04:00',
@@ -94,15 +98,15 @@ export const SECOND_EVENTS = [
   },
   */
   {
-    name: '03:00PM - SBCS Event',
+    name: '03:00PM - iOS Development Event with SBCS',
     endTime: '2021-09-25T16:00:00.000-04:00',
   },
   {
-    name: '04:00PM - WiCS Event',
+    name: '04:00PM - Android Development Event with WiCS\n04:00PM - Submissions Due',
     endTime: '2021-09-25T17:00:00.000-04:00',
   },
   {
-    name: '05:00PM - Judging Begins + Submission Deadline',
+    name: '05:00PM - Judging Begins',
     endTime: '2021-09-25T18:00:00.000-04:00',
   },
   {
