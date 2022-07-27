68c6c0953c659ef80ad07e65e2e5cfcee1d1417d
Jenny Bao <jennybao1004@gmail.com>
2022-07-26 21:17:58 -0400

    Add Libraries to sponsor

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sponsors.css';
 import SchifferLogo from '../../assets/sponsors/Schiffer_Icon.svg';
+import LibraryLogo from '../../assets/sponsors/Libraries_Icon.png';
 
 // The Sponsors page
 function Sponsors() {
@@ -10,6 +11,7 @@ function Sponsors() {
       <div className="sponsors-wrapper">
         <div className="golden-sponsor">
           <img src={SchifferLogo} className="golden-logo" alt="schiffer-logo" />
+          <img src={LibraryLogo} className="golden-logo" alt="library-logo" />
         </div>
       </div>
     </div>
