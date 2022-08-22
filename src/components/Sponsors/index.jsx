9ff7d5c3d8f81460d5e1af225f74464e16df25bd
Jenny Bao <jennybao1004@gmail.com>
2022-08-22 18:28:55 -0400

    Add Standout Stickers logo

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './sponsors.css';
 import SchifferLogo from '../../assets/sponsors/Schiffer_Icon.svg';
 import LibraryLogo from '../../assets/sponsors/Libraries_Icon.png';
+import StandoutLogo from '../../assets/sponsors/Standout_Icon.png';
 
 // The Sponsors page
 function Sponsors() {
@@ -13,6 +14,7 @@ function Sponsors() {
           <img src={SchifferLogo} className="partner-logo-stacked" alt="schiffer-logo" />
           <img src={LibraryLogo} className="partner-logo" alt="library-logo" />
           <img src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-color.svg" className="partner-logo-stacked" alt="mlh-logo" />
+          <img src={StandoutLogo} className="partner-logo-stacked" alt="library-logo" />
         </div>
       </div>
     </div>
