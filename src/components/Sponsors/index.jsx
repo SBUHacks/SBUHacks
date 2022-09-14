d24c514b4745816fcc9e96091a673ad20d9405ca
Jenny Bao <jennybao1004@gmail.com>
2022-09-13 20:32:28 -0400

    Add Echo3D, Resideo, & Zebra logos

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -3,6 +3,9 @@ import './sponsors.css';
 import SchifferLogo from '../../assets/sponsors/Schiffer_Icon.svg';
 import LibraryLogo from '../../assets/sponsors/Libraries_Icon.png';
 import StandoutLogo from '../../assets/sponsors/Standout_Icon.svg';
+import ZebraLogo from '../../assets/sponsors/Zebra_Icon.jpg';
+import Echo3DLogo from '../../assets/sponsors/echo3D_Icon.png';
+import ResideoLogo from '../../assets/sponsors/Resideo_Icon.svg';
 
 // The Sponsors page
 function Sponsors() {
@@ -16,6 +19,9 @@ function Sponsors() {
           <a className="partner-logo-stacked" href="https://mlh.io/">
             <img src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-color.svg" alt="mlh-logo" />
           </a>
+          <img src={Echo3DLogo} className="partner-logo" alt="library-logo" />
+          <img src={ResideoLogo} className="partner-logo" alt="library-logo" />
+          <img src={ZebraLogo} className="partner-logo" alt="library-logo" />
           <a href="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-spring2022&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intros">
             <img src={StandoutLogo} id="standout-logo" alt="standout-logo" />
           </a>
