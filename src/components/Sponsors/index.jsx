a00ecf54de09013c1dfc5c5504715fb1499dd113
Jenny Bao <jennybao1004@gmail.com>
2022-09-15 17:27:32 -0400

    Organize sponsors

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -6,6 +6,7 @@ import StandoutLogo from '../../assets/sponsors/Standout_Icon.svg';
 import ZebraLogo from '../../assets/sponsors/Zebra_Icon.jpg';
 import Echo3DLogo from '../../assets/sponsors/echo3D_Icon.png';
 import ResideoLogo from '../../assets/sponsors/Resideo_Icon.svg';
+import SBUCSLogo from '../../assets/sponsors/SBU_CS_Icon.gif';
 
 // The Sponsors page
 function Sponsors() {
@@ -15,16 +16,17 @@ function Sponsors() {
       <div className="sponsors-wrapper">
         <div className="sponsors-partners">
           <img src={SchifferLogo} className="partner-logo-stacked" alt="schiffer-logo" />
-          <img src={LibraryLogo} className="partner-logo" alt="library-logo" />
+          <img src={ResideoLogo} className="partner-logo" alt="resideo-logo" />
+          <img src={ZebraLogo} className="partner-logo" alt="zebra-logo" />
           <a className="partner-logo-stacked" href="https://mlh.io/">
             <img src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-color.svg" alt="mlh-logo" />
           </a>
           <img src={Echo3DLogo} className="partner-logo" alt="library-logo" />
-          <img src={ResideoLogo} className="partner-logo" alt="library-logo" />
-          <img src={ZebraLogo} className="partner-logo" alt="library-logo" />
-          <a href="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-spring2022&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intros">
+          <a href="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-spring2022&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intros" className="partner-logo">
             <img src={StandoutLogo} id="standout-logo" alt="standout-logo" />
           </a>
+          <img src={LibraryLogo} className="partner-logo" alt="library-logo" />
+          <img src={SBUCSLogo} className="partner-logo" alt="cs-logo" />
         </div>
       </div>
     </div>
