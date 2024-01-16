f0a9f8f55edccc187a8d8562c359a1c75c3563e8
Jenny Bao <jennybao1004@gmail.com>
2024-01-15 21:36:19 -0500

    add standout stickers sponsor

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sponsors.css';
 // import SchifferLogo from '../../assets/sponsors/Schiffer_Icon.svg';
-// import StandoutLogo from '../../assets/sponsors/Standout_Icon.svg';
+import StandoutLogo from '../../assets/sponsors/Standout_Icon.svg';
 // import ZebraLogo from '../../assets/sponsors/Zebra_Icon.jpg';
 // import Echo3DLogo from '../../assets/sponsors/echo3D_Icon.png';
 // import ResideoLogo from '../../assets/sponsors/Resideo_Icon.svg';
@@ -16,9 +16,7 @@ function Sponsors() {
       <span className="section-title general-style">Partners</span>
       <div className="sponsors-wrapper">
         <div className="sponsors-partners">
-          <div className="coming-soon">Coming Soon...</div>
-          {/*
-          <img src={SchifferLogo} className="partner-logo-stacked" alt="schiffer-logo" />
+          {/* <img src={SchifferLogo} className="partner-logo-stacked" alt="schiffer-logo" />
           <img src={ResideoLogo} className="partner-logo" alt="resideo-logo" />
           <img src={ZebraLogo} className="partner-logo" alt="zebra-logo" />
           <img src={GoogleCloudLogo} className="partner-logo" alt="library-logo" />
@@ -27,11 +25,11 @@ function Sponsors() {
           <a className="partner-logo-stacked" href="https://mlh.io/">
             <img src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-color.svg" alt="mlh-logo" />
           </a>
-          <img src={SBULogo} className="partner-logo" alt="cs-logo" />
-          <a href="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-spring2022&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intros" className="partner-logo">
+          <img src={SBULogo} className="partner-logo" alt="cs-logo" /> */}
+          <a href="http://hackp.ac/mlh-StandOutStickers-hackathons" className="partner-logo">
             <img src={StandoutLogo} id="standout-logo" alt="standout-logo" />
           </a>
-          */}
+          <div className="coming-soon">Coming Soon...</div>
         </div>
       </div>
     </div>
